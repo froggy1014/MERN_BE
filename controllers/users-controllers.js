f476fc21e906db00f9f3130b0c559cc270dca3e5
froggy1014 <clcl6084@gmail.com>
2022-12-14 19:52:09 +0900

    feat[login]: password check through bcrypt.compare

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -90,11 +90,23 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError('Invalid Credentials, could not log you in.', 401);
     return next(error);
   }
   
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password)
+  } catch {
+    const error = new HttpError('Could not log you in, please check yout ceredentials and try agai.', 500);
+    return next(error);
+  }
+
+  if(!isValidPassword) {
+    const error = new HttpError('Invalid Credentials, could not log you in.', 401);
+    return next(error);
+  } 
 
   res.json({ message: 'Logged in!', user : existingUser.toObject({getters: true}) });
 };
