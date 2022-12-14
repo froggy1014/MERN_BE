70aae7f97741bbc3bf2dfe007b2117eaa9c386f1
froggy1014 <clcl6084@gmail.com>
2022-12-14 22:25:19 +0900

    feat: create middleware for checking auth header

---
+++ middleware/check-auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  
+  if(req.method === 'OPTIONS'){
+    return next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+    if(!token) {
+      throw new Error('Authentication failed');
+    }
+
+    // Secret Key를 통해 유효성을 검사한다.  
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+
+    // 이런식으로 미들웨어 중간에서 req에 객체에 데이터를 담아 넘겨줄 수 있다.
+    req.userData = {userId: decodedToken.userId};
+    next();
+  } catch {
+    const error = new HttpError('Authentication failed', 401);
+    return next(error);
+  }
+}
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -5,11 +5,15 @@ const placeControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 const fileUpload = require('../middleware/file-upload');
-
+const checkAuth = require('../middleware/check-auth');
 
 router.get('/:pid', placeControllers.getPlaceById);
 
 router.get('/user/:uid', placeControllers.getPlaceByUserId);
+
+
+// post, patch 라우팅에 앞서 jwt 토큰 유무를 확인해줄 미들웨어를 추가해준다.
+router.use(checkAuth);
 
 router.post(
   '/',
